dcb940cc8597ad69864d48d2ff6979a64eb9c55e
yukun.liu <yukun.liu@bianlifeng.com>
2019-01-05 18:40:59 +0800

    指标维度细化

--- monitor-client/src/main/java/org/vitoliu/common/metrics/metric/impl/DefaultMetricRegistry.java
+++ monitor-client/src/main/java/org/vitoliu/common/metrics/metric/impl/DefaultMetricRegistry.java
@@ -1,7 +1,12 @@
 package org.vitoliu.common.metrics.metric.impl;
 
 import java.util.Iterator;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
 
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.google.common.collect.Maps;
 import org.vitoliu.ccommon.metrics.config.MetricConfig;
 import org.vitoliu.common.metrics.metric.MetricItem;
 import org.vitoliu.common.metrics.metric.MetricRegistry;
@@ -15,28 +20,36 @@ public class DefaultMetricRegistry implements MetricRegistry {
 
 	private static final DefaultMetricRegistry INSTANCE = new DefaultMetricRegistry();
 
+	private ConcurrentMap<MetricConfig, MetricItem> metricItemMap = Maps.newConcurrentMap();
 
-	public static DefaultMetricRegistry getInstance() {
+	private Supplier<Integer> sizeSupplier = Suppliers.memoizeWithExpiration(() -> metricItemMap.size(), 1, TimeUnit.MINUTES);
+
+	private DefaultMetricRegistry() {
+	}
+
+
+	static MetricRegistry getInstance() {
 		return INSTANCE;
 	}
 
 	@Override
 	public MetricItem get(MetricConfig metricConfig) {
-		return null;
+		return metricItemMap.get(metricConfig);
 	}
 
 	@Override
 	public void register(MetricItem metricItem) {
-
+		MetricConfig metricConfig = metricItem.getMetricConfig();
+		metricItemMap.putIfAbsent(metricConfig, metricItem);
 	}
 
 	@Override
 	public int size() {
-		return 0;
+		return sizeSupplier.get();
 	}
 
 	@Override
 	public Iterator<MetricItem> iterator() {
-		return null;
+		return metricItemMap.values().iterator();
 	}
 }
